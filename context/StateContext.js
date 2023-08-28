5b0f3e0424d6ae6afab0655d0a2e4d970f44e8ef
Sharon Wong <s.wongx0@gmail.com>
2023-08-28 16:55:10 -0700

    Implement cart logic

--- context/StateContext.js
+++ context/StateContext.js
@@ -5,10 +5,31 @@ const Context = createContext();
 
 export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false);
-    const [cartItems, setCartItems] = useState();
-    const [totalPrice, setTotalPrice] = useState();
-    const [totalQuantities, setTotalQuantities] = useState();
+    const [cartItems, setCartItems] = useState([]);
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
+    const onAdd = (product, quantity) => {
+        const checkProductInCart = cartItems.find((item) => item._id === product._id);
+        setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity)
+
+        if (checkProductInCart) {
+            const updatedCartItems = cartItems.map((cartProduct) => {
+                if (cartProduct._id === product._id) return {
+                    ...cartProduct,
+                    quantity: cartProduct.quantity + quantity
+                }
+            })
+
+            setCartItems(updatedCartItems);
+        } else {
+            product.quantity = quantity;
+
+            setCartItems([...cartItems, { ...product }]);
+        }
+        toast.success(`${qty} ${product.name} added to the cart.`);
+    }
 
     const incQty = () => {
         setQty((prevQty) => prevQty + 1)
@@ -25,12 +46,14 @@ export const StateContext = ({ children }) => {
         <Context.Provider
             value={{
                 showCart,
+                setShowCart,
                 cartItems,
                 totalPrice,
                 totalQuantities,
                 qty,
                 incQty,
                 decQty,
+                onAdd,
             }}>
             {children}
         </Context.Provider>
